85d3ba6a90a3a7389cfa426252f24b47233add3c
renechoi <renechoi90@gmail.com>
2023-03-05 14:13:37 +0900

    feat: bean validation by seperated objects

---
+++ src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -0,0 +1,26 @@
+package hello.itemservice.web.validation.form;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class ItemUpdateForm {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotBlank
+    @Range(min=1000, max=1000000)
+    private Integer price;
+
+    @NotNull
+    @Max(value =9999)
+    private Integer quantity;
+}
